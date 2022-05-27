f2deeaa8a7cbb4dbb4dad8945fd24599522496ee
HilliamT <hilliamtung@gmail.com>
2022-05-27 19:50:36 +0100

    feat: added savings calculator component placeholder

---
+++ client/src/components/SavingsCalculator.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SavingsCalculator = () => {
+    return (
+        <div>
+            <h1>Savings Calculator</h1>
+        </div>
+    );
+}
+
+export default SavingsCalculator;
